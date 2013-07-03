4b099e4c1ebc0fa9cb4b779f51465510fb33bf55
Lee "MadHat" Heath <madhat@unspecific.com>
2013-07-03 09:40:24 -0500

    Template for Messaging module

---
+++ include/Pasteater/Messaging.pm
@@ -0,0 +1,12 @@
+#!/bin/perl
+# Program Name: Core Module for Pasteater
+# Created: 2013-07-01
+# Author: Lee "MadHat" Heath
+# Version: 0.1 (2013-07-03)
+# Package: Pasteater::Messaging
+
+package Pasteater::Messaging;
+
+use strict;
+
+1;
